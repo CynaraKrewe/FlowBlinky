b74dd52543c766c4ec163cf0a63a9d44f20e8ee7
m-spiessens <mathias@spiessens.eu>
2018-04-05 20:43:04 +0200

    Upgrade to FlowTM4C 1.3 stable.

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,9 @@
 from conans import ConanFile
 
 class ExampleUsingFlowTM4C(ConanFile):
-   requires = "FlowTM4C/1.2@spiessensm/stable"
+   requires = "FlowTM4C/1.3@spiessensm/stable"
    
    def imports(self):
       self.copy("*.h")
       self.copy("*.cpp")
       self.copy("*.a")
-      self.copy(".project")
-      self.copy(".cproject")
